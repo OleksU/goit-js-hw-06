5c750d8dadce9f60926e29f290e399b077d4a28c
OleksU <cibulskiy089@gmail.com>
2024-01-25 19:36:44 +0100

    updates

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+class Storage {
+    #item;
+    constructor ([item]){
+        this.#item = item;
+    }
+    
+    getItems(){
+        return this.#item;
+    }
+    addItem(newItem) {
+        return this.item.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const itemRemove = this.#item.filter(itemToRemove);
+        return itemRemove !== itemToRemove;
+    }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
